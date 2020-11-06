417dfe8a520df619d5af0825ab38cbeac9356572
Grant Hesketh <ghesketh76@gmail.com>
2020-11-06 19:24:31 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {
           name: "Benvolio",
           age: "17",
-          status: "worried"
+          attitude: "worried"
         },
         {
           name: "Mercutio",
